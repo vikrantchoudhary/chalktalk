55fd5b90004a6fc3506d5f5bfe849b2170170631
vikrant_kc <vikrantk@ndtv.com>
2022-01-10 23:13:37 +0530

    hero  fixes

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -7,24 +7,30 @@ const Hero = () => {
             <div className="content">
                 <h1>Welcome to our Chalk and Talk World  </h1>
                 <div className="section">
-                    We are here to help you to overcome your speech,hearing and learning disablities
+                  We work closely to assess hearing and speech problems, diagnosing and recommending a plan to tackle them.
+                  Qualified and skilled speech-language therapists and audiologists who evaluate and treat patients suffering from language, speech and fluency problems.
+                  We can diagnose, test and treat people suffering from speech pathology problems. 
+                  If there are any underlying medical problems to speech and pathology issues that would also be identified and corresponding treatment started along with speech and hearing therapy. 
+                  Oral motor, cognitive-linguistic, swallowing, hearing, speech, and even language disorders might actually be the result of neurological events which would be identified and and treatment suggested. 
+                  Speech therapists would conduct standard language tests coupled with cognitive-linguistic tests to provide accurate treatment to the patients. 
+                  
                 </div>
                 <div className="allservices">
                     Our services includes 
                     <div className="services" >
                         <h3>speech therapy</h3>
                         <ul className="Speech">
-                            <li><a href='#'>S1</a></li>
-                            <li><a href='#'>S2</a></li>
-                            <li><a href='#'>S3</a></li>
-                            <li><a href='#'>S3</a></li>
+                            <li>S1</li>
+                            <li>S2</li>
+                            <li>S3</li>
+                            <li>S4</li>
                         </ul>
                         <h3>Hearing treatments</h3>
                         <ul className="Hearing">
-                            <li><a href='#'>H1</a></li>
-                            <li><a href='#'>H2</a></li>
-                            <li><a href='#'>H3</a></li>
-                            <li><a href='#'>H4</a></li>
+                            <li>H1</li>
+                            <li>H2</li>
+                            <li>H3</li>
+                            <li>H4</li>
                         </ul>
                     </div>
                 </div>
